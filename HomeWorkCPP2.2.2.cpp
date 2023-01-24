cc9db335f7a80ef0a0ca1987d620dfc0b872f1ac
W-AndreyV-W <117563643+W-AndreyV-W@users.noreply.github.com>
2023-01-25 00:30:36 +0500

    first commit

---
+++ HomeWorkCPP2.2.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+//#include <locale.h>
+#include <windows.h>
+
+struct bank {
+    long long score;
+    std::string name;
+    long money;
+};
+
+void dep(bank& owner, long& newmoney) {
+    owner.money = newmoney;
+}
+
+int main()
+{
+    long newmoney = 0;
+    bank owner;
+    //setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Введите номер счёта: ";
+    std::cin >> owner.score;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> owner.name;
+    std::cout << "Введите баланс: ";
+    std::cin >> owner.money;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newmoney;
+    dep(owner, newmoney);
+    std::cout << "Ваш счёт: " << owner.name << ", " << owner.score <<", " << owner.money << std::endl;
+}
